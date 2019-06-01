fdf3dbdb47acd3b2fc603d6f36fa9dab98830b01
MariaEfremova <mp.efremova@yandex.ru>
2019-06-01 12:19:56 +0300

    second commit

--- 20.sh
+++ 20.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-cd ..
-ls
-cd john/
-ls
-rm -rf Desktop
-rm -rf Documents
-rm file1
-rm file2
-rm file3
-cd ..
-ls
-cd suzen/
-ls | egrep -w "[A-Za-z0-9]{28}"
+rm -r /home/john
+ls /home/suzen
